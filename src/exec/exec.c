361afa7fa9310bfde6d8667cd314d46cc326f5b2
Philipp Nolte <pnolte@1-D-32.42heilbronn.de>
2023-01-17 15:58:46 +0100

    something changed

--- src/exec/exec.c
+++ src/exec/exec.c
@@ -6,7 +6,7 @@
 /*   By: pnolte <pnolte@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 15:17:53 by pnolte            #+#    #+#             */
-/*   Updated: 2023/01/17 11:28:37 by pnolte           ###   ########.fr       */
+/*   Updated: 2023/01/17 15:46:55 by pnolte           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,28 @@
 
 t_env		**g_envp;
 
+char **command
+
 void	executer(char **list)
 {
 	int i;
+	char
 	
 	i = 0;
-	while (i)
+	while (list[i] != NULL)
 	{
-		redirection()
-		check_for_pipes()
-			redirection()
-			fork();
-				if (builtin)
-					exec_builtin();
-				if (!buitlin)
-					search_path()
-					fork();
-					execv();
-					wait();	
-			wait()			
+		while ()
+		// redirection()
+		// check_for_pipes()
+			// redirection()
+			// fork();
+			// 	if (builtin)
+			// 		exec_builtin();
+			// 	if (!buitlin)
+			// 		search_path()
+			// 		fork();
+			// 		execv();
+			// 		wait();	
+			// wait()			
 	}
 }
--- src/exec/exec_test.c
+++ src/exec/exec_test.c
@@ -6,7 +6,7 @@
 /*   By: pnolte <pnolte@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 15:17:53 by pnolte            #+#    #+#             */
-/*   Updated: 2023/01/16 17:58:43 by pnolte           ###   ########.fr       */
+/*   Updated: 2023/01/17 12:28:43 by pnolte           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int main()
 {
-	char *input[] = {"cd", "..", "ls", "", "echo", "-n", NULL};
+	char *input[] = {"cd", "..", "\x19", "ls", "", "\x19", "echo", "-n", "\x19", "cat", ">", "tmp", "\x19", NULL};
 	
 	if (init_env() == EXIT_FAILURE)
 		return (EXIT_FAILURE);
