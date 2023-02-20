8453e26510f235eb5d6c278b0ff410e13cc89708
amorvai <amorvai@student.42heilbronn.de>
2023-02-20 23:54:31 +0100

    added some to libft

--- lib/the_lib/libft/ft_strncmp.c
+++ lib/the_lib/libft/ft_strncmp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pnolte <pnolte@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*   By: amorvai <amorvai@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/30 13:53:17 by amorvai           #+#    #+#             */
-/*   Updated: 2023/01/13 17:13:41 by pnolte           ###   ########.fr       */
+/*   Updated: 2023/02/20 23:38:24 by amorvai          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,11 +40,11 @@ int	ft_strcmp(const char *s1, const char *s2)
 
 	t = 0;
 	if (s1 == NULL && s2 == NULL)
-		return(0);
+		return (0);
 	else if (s1 == NULL)
-		return(*(unsigned char *)s2);
+		return (*(unsigned char *)s2);
 	else if (s2 == NULL)
-		return(*(unsigned char *)s2);
+		return (*(unsigned char *)s1);
 	while (s1[t] == s2[t])
 	{
 		if (s1[t] == '\0' && s2[t] == '\0')
