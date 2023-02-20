aff2dff5b925fa930bcafc96042d74c893a0bb59
Philipp Nolte <phipno@gmail.com>
2023-02-20 16:43:12 +0100

    worked on permission fixed executables, now redoing cd

--- src/builtin/bi_cd.c
+++ src/builtin/bi_cd.c
@@ -6,7 +6,7 @@
 /*   By: pnolte <pnolte@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 19:02:07 by pnolte            #+#    #+#             */
-/*   Updated: 2023/02/17 16:26:45 by pnolte           ###   ########.fr       */
+/*   Updated: 2023/02/20 16:40:49 by pnolte           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,57 +24,59 @@ static char *cd_cut(char *path);
 void bi_cd(char *path)
 {
 	char		*str;
-	struct stat	path_stat;
 	char		*cwd;
+	struct stat	s;
 	
 	str = NULL;
 	cwd = check_for_cwd();
-	stat(path, &path_stat);
+	stat(path, &s);
 	if (path[0] == '.' && path[1] == '\0')
 		return;
 	else if (path == NULL || (path != NULL && path[0] == '~' && path[1] == '\0'))
-		add_env(ft_strjoin("PWD=", get_env("HOME")));
-	else if (path[0] == '.' && path[1] == '.' && path[2] == '\0')
-	{
-		str = cd_cut(cwd);
-		add_env(ft_strjoin("PWD=", str));
-	}
-	else if ((path_stat.st_mode & S_IFMT) == S_IFREG)
-		printf("MML: cd: %s: Not a directory:\n", path);
-	else if (path != NULL)
-		bi_cd2(path, cwd);
-	else
-		printf("MML: cd: %s: Something went not as planned\n", str);
+		chdir("")
+	
+	
+	// else if (path[0] == '.' && path[1] == '.' && path[2] == '\0')
+	// {
+	// 	str = cd_cut(cwd);
+	// 	add_env(ft_strjoin("PWD=", str));
+	// }
+	// else if ((s.st_mode & S_IFMT) == S_IFREG)
+	// 	printf("MML: cd: %s: Not a directory:\n", path);
+	// else if (path != NULL)
+	// 	bi_cd2(path, cwd);
+	// else
+	// 	printf("MML: cd: %s: Something went not as planned\n", str);
 }
 
-static void	bi_cd2(char *path, char *cwd)
-{
-	char *str;
-	char *str2;
+// static void	bi_cd2(char *path, char *cwd)
+// {
+// 	char *str;
+// 	char *str2;
 	
-	if (path[0] != '/')
-	{
-		str2 = ft_strjoin("/", path);
-		str = ft_strjoin(cwd, str2);
-		free(str2);
-		if (chdir(str) == -1)
-		{
-			printf("MML: cd: %s: No such file or directory:\n", path);
-			return ;
-		}	
-		add_env(ft_strjoin("PWD=", str));
-		free(str);
-	}
-	else if (path[0] == '/')
-	{
-		if (chdir(path) == -1)
-		{
-			printf("MML: cd: %s: No such file or directory:\n", path);
-			return ;
-		}	
-		add_env(ft_strjoin("PWD=", path));
-	}
-}
+// 	if (path[0] != '/')
+// 	{
+// 		str2 = ft_strjoin("/", path);
+// 		str = ft_strjoin(cwd, str2);
+// 		free(str2);
+// 		if (chdir(str) == -1)
+// 		{
+// 			printf("MML: cd: %s: No such file or directory:\n", path);
+// 			return ;
+// 		}	
+// 		add_env(ft_strjoin("PWD=", str));
+// 		free(str);
+// 	}
+// 	else if (path[0] == '/')
+// 	{
+// 		if (chdir(path) == -1)
+// 		{
+// 			printf("MML: cd: %s: No such file or directory:\n", path);
+// 			return ;
+// 		}	
+// 		add_env(ft_strjoin("PWD=", path));
+// 	}
+// }
 
 static char *check_for_cwd()
 {
@@ -82,34 +84,34 @@ static char *check_for_cwd()
 	
 	cwd = get_env("PWD");
 	if (get_env("HOME") == NULL || ft_strcmp(get_env("HOME"), "") == 0)
-		add_env(ft_strjoin("HOME=", "/"));
+		
 	if (cwd == NULL || ft_strcmp(cwd, "") == 0)
 	{
 		if (get_env("OLDPWD") == NULL || ft_strcmp(get_env("OLDPWD"), "") == 0)
-			add_env(ft_strjoin("PWD=", "/"));
+			add_env(ft_strdup("PWD=/"));
 		cwd = get_env("OLDPWD");
 	}
 	return (cwd);
 }
 
-static char *cd_cut(char *path)
-{
-	char	*shortend_path;
-	int		i;
-	int		j;
+// static char *cd_cut(char *path)
+// {
+// 	char	*shortend_path;
+// 	int		i;
+// 	int		j;
 	
-	i = ft_strlen(path);
-	while (path[i] != '/')
-		i--;
-	shortend_path = malloc(sizeof(char) * i + 1);
-	if (shortend_path == NULL)
-		return (NULL);
-	j = 0;
-	while (j < i)
-	{
-		shortend_path[j] = path[j];
-		j++;	
-	}
-	shortend_path[j] = '\0';
-	return(shortend_path);
-}
+// 	i = ft_strlen(path);
+// 	while (path[i] != '/')
+// 		i--;
+// 	shortend_path = malloc(sizeof(char) * i + 1);
+// 	if (shortend_path == NULL)
+// 		return (NULL);
+// 	j = 0;
+// 	while (j < i)
+// 	{
+// 		shortend_path[j] = path[j];
+// 		j++;	
+// 	}
+// 	shortend_path[j] = '\0';
+// 	return(shortend_path);
+// }
--- src/exec/exec.h
+++ src/exec/exec.h
@@ -6,7 +6,7 @@
 /*   By: pnolte <pnolte@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 16:27:00 by pnolte            #+#    #+#             */
-/*   Updated: 2023/02/16 16:28:24 by pnolte           ###   ########.fr       */
+/*   Updated: 2023/02/20 14:56:40 by pnolte           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 # include "../structure/command.h"
 
-void	executer(t_simp_com *head);
+int		executer(t_simp_com *head);
 int		where_ma_redirec(t_simp_com *single_cmd);
 void	decisionmaker(char **simple_cmd, char *flex);
 void	multiple_pipes(t_simp_com *head, int amo_pipes);
