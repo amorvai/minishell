26563cbc5a2ca07120d934d0af887ca720753219
amorvai <amorvai@student.42heilbronn.de>
2023-02-20 23:58:50 +0100

    fixed norm in lib.h header

--- lib/the_lib/lib.h
+++ lib/the_lib/lib.h
@@ -1,9 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lib.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amorvai <amorvai@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/20 23:57:12 by amorvai           #+#    #+#             */
+/*   Updated: 2023/02/20 23:57:38 by amorvai          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef LIB_H
 # define LIB_H
 
-#include "libft/libft.h"
-#include "printf/ft_printf.h"
-#include "get_next_line/get_next_line_fd.h"
+# include "libft/libft.h"
+# include "printf/ft_printf.h"
+# include "get_next_line/get_next_line_fd.h"
 
 #endif
