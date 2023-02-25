39c5954dc98189f483441fd3102955158bed44e2
amorvai <amorvai@student.42heilbronn.de>
2023-02-25 11:39:31 +0100

    added grab_cwd function that returns cwd

--- src/builtin/bi_pwd.c
+++ src/builtin/bi_pwd.c
@@ -3,18 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   bi_pwd.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pnolte <pnolte@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*   By: amorvai <amorvai@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 19:02:07 by pnolte            #+#    #+#             */
-/*   Updated: 2023/02/17 17:01:40 by pnolte           ###   ########.fr       */
+/*   Updated: 2023/02/25 11:28:54 by amorvai          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../env/env.h"
 #include "../../lib/the_lib/lib.h"
+#include <unistd.h>
+
+#include <limits.h>
+
+#ifndef PATH_MAX
+# define PATH_MAX 1024
+#endif
+
+char	*grab_cwd()
+{
+	char	buf[PATH_MAX];
+
+	if (getcwd(buf, sizeof(buf)))
+		return (buf);
+	return (NULL);
+}
 
 void bi_pwd()
 {
-	ft_putstr_fd(get_env("PWD"), 1);
+	ft_putstr_fd(grab_cwd(), 1);
 	ft_putchar_fd('\n', 1);
 }
--- src/builtin/builtins.h
+++ src/builtin/builtins.h
@@ -3,15 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   builtins.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pnolte <pnolte@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*   By: amorvai <amorvai@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 18:58:59 by pnolte            #+#    #+#             */
-/*   Updated: 2023/02/20 16:53:13 by pnolte           ###   ########.fr       */
+/*   Updated: 2023/02/25 11:14:11 by amorvai          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUILTINS_H
 # define BUILTINS_H
+
+char	*grab_cwd();
 
 void	bi_echo(char **simple_command);
 int		bi_cd(char *path);
