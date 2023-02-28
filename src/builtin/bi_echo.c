68ffbdc94ac69a4a5e6cf13c56126833e07b2bc5
amorvai <amorvai@student.42heilbronn.de>
2023-02-28 18:35:58 +0100

    echo fix

--- src/builtin/bi_echo.c
+++ src/builtin/bi_echo.c
@@ -3,53 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   bi_echo.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pnolte <pnolte@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*   By: amorvai <amorvai@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 19:02:07 by pnolte            #+#    #+#             */
-/*   Updated: 2023/02/28 14:29:32 by pnolte           ###   ########.fr       */
+/*   Updated: 2023/02/28 18:33:43 by amorvai          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../env/env.h"
 #include "../../lib/the_lib/lib.h"
 #include <stdio.h>
-
-static void	bi_echo2(char **simple_command);
+#include <stdbool.h>
 
 void	bi_echo(char **simple_command)
 {
-	int	i;
+	int		i;
+	bool	flag;
 
-	if (ft_strcmp(simple_command[1], "-n") != 0)
+	i = 1;
+	flag = true;
+	while (simple_command[i] != NULL && !ft_strcmp(simple_command[i], "-n"))
 	{
-		i = 1;
-		while (simple_command[i] != NULL)
-		{
-			ft_putstr_fd(simple_command[i], 1);
-			if (simple_command[i + 1] != NULL)
-				ft_putchar_fd(' ', 1);
-			i++;
-		}
-		ft_putchar_fd('\n', 1);
-	}
-	else
-		bi_echo2(simple_command);
-	add_env(ft_strdup("?=0"));
-}
-
-static void	bi_echo2(char **simple_command)
-{
-	int	i;
-
-	i = 2;
-	while (simple_command[i] != NULL)
-	{
-		if (ft_strcmp(simple_command[i], "-n") != 0)
-		{
-			ft_putstr_fd(simple_command[i], 1);
-			if (simple_command[i + 1] != NULL)
-				ft_putchar_fd(' ', 1);
-		}
+		flag = false;
 		i++;
 	}
+	while (simple_command[i] != NULL)
+	{
+		ft_putstr_fd(simple_command[i], 1);
+		if (simple_command[i + 1] != NULL)
+			ft_putchar_fd(' ', 1);
+		i++;
+	}
+	if (flag)
+		ft_putchar_fd('\n', 1);
+	add_env(ft_strdup("?=0"));
 }
